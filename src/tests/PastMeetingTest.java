053ba0b86ae0b3d32521459f40e2e823911a6316
Hannah Spencer <hspenc03@dcs.bbk.ac.uk>
2017-02-25 12:10:22 +0000

    Added constructor & calendar for tests

--- src/tests/PastMeetingTest.java
+++ src/tests/PastMeetingTest.java
@@ -1,10 +1,15 @@
 package tests;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 import org.junit.*; 
 import static org.junit.Assert.*;
 import org.junit.Before;
 
+import implementations.PastMeetingImpl;
+
 public class PastMeetingTest {
+	
 	PastMeetingImpl test;
 	Set<Contact> contacts;
 	Calendar date;
@@ -12,17 +17,28 @@ public class PastMeetingTest {
 	
 	@Before
 	public void setUp() {
-		test = new PastMeetingImpl();
+		date = new GregorianCalendar(2016,9,20);
+		test = new PastMeetingImpl(date,contacts,"Here are some notes");
 	}
+	
+	@Test
+	public void checkConstructorAndGetters() {
+    	assertTrue(test.getId() != 0);
+    	assertTrue(test.getId() > 0);
+    	assertTrue(test.getDate().equals(date));
+    	assertTrue(test.getContacts().equals(contacts));
+	}
+	
+	
 	@Test
 	public void checkGetNotes_WithNotes() {
-		test.notes = "Example";
 		String output = test.getNotes();
-		assertTrue(output.equals("Example"));
+		assertTrue(output.equals("Here are some notes"));
 	}
 	
 	@Test
 	public void checkGetNotes_NoNotes() {
+		PastMeetingImpl emptyNoteTest = new PastMeetingImpl(date, contacts, "");
 		String output = test.getNotes();
 		assertTrue(output.equals(""));
 	}

778603b6607fe41a3e3a7f25991aad134fd4f333
Hannah Spencer <hspenc03@dcs.bbk.ac.uk>
2017-02-25 13:34:54 +0000

    Update MeetingTest

--- src/tests/MeetingTest.java
+++ src/tests/MeetingTest.java
@@ -1,29 +1,43 @@
 package tests;
+import implementations.FutureMeetingImpl;
+import implementations.MeetingImpl;
+import interfaces.Contact;
+import interfaces.FutureMeeting;
+import interfaces.Meeting;
+
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Set;
+
 import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
 public class MeetingTest {
 	
-	Meeting test = new MeetingImpl();
+	Meeting test;
+	Calendar date;
+	Set<Contact> contacts;
+	
+	@Before
+	public void setUp() {
+		date = new GregorianCalendar(2017,9,20);
+		test = new MeetingImpl(date, contacts);
+	}
 
 	@Test
-	public void testGetId() {
+	public void testGetters() {
     	assertTrue(test.getId() != 0);
-    	assertTrue(test.getId() > 0);
+    	assertTrue(test.getId() > 1000);
+    	assertTrue(test.getDate().equals(date));
+    	assertTrue(test.getContacts().equals(contacts));
 	}
 	
 	@Test
-	public void testGetDate() {
-		Calendar testCal;
-		testCal = Calendar.getInstance();
-		Calendar date = test.getDate();
-		date.add(Calendar.YEAR, 1);
-		assertFalse(date.get(Calendar.YEAR))
-	}
-	
-	@Test
-	public void testGetContacts(){
-		//check getContacts
+	public void testDate() {
+		assertEquals(2017,test.getDate().get(Calendar.YEAR));
+		assertEquals(9, test.getDate().get(Calendar.MONTH));
+		assertEquals(20, test.getDate().get(Calendar.DATE));
 	}
 
 }

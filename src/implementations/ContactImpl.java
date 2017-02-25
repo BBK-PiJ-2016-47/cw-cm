8aa194ac772182141eb54c1b62601d838e63bb63
Hannah Spencer <hspenc03@dcs.bbk.ac.uk>
2017-02-25 15:26:17 +0000

    Tidied up and happy with ContactImpl

--- src/implementations/ContactImpl.java
+++ src/implementations/ContactImpl.java
@@ -2,24 +2,36 @@ package implementations;
 import interfaces.Contact;
 
 public class ContactImpl implements Contact {
-	  private int id;
-	  private String name;
-	  private String notes;
+	private int id;
+	private String name;
+	private String notes;
+	private static int count;
 	  
-	  public int getId() {
-	    return id;
-	  }
-	  
-	  public String getName() {
-	    return name;
-	  }
-	  
-	  public String getNotes() {
-	    return notes;
-	  }
-	  
-	  public void addNotes(String note) {
-	    this.notes = note;
-	  }
+	public ContactImpl(String name, String notes){
+		this.name = name;
+		this.notes = notes;
+		count++;
+		this.id = count;
+	}
+	
+	@Override
+	public int getId() {
+		return id;
+	}
+	
+	@Override
+	public String getName() {
+		return name;
+	}
+	
+	@Override
+	public String getNotes() {
+		return notes;
+	}
+	
+	@Override
+	public void addNotes(String note) {
+		this.notes = note;
+	}
 
 }

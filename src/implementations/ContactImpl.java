e465ef31174ee1c335c9d285d75e9932c4ce0995
Hannah Spencer <hspenc03@dcs.bbk.ac.uk>
2017-03-01 13:19:21 +0000

    Add exceptions for constructor

--- src/implementations/ContactImpl.java
+++ src/implementations/ContactImpl.java
@@ -1,17 +1,33 @@
 package implementations;
+import java.util.HashSet;
+import java.util.Set;
+
 import interfaces.Contact;
 
 public class ContactImpl implements Contact {
 	private int id;
 	private String name;
 	private String notes;
-	private static int count;
+	private Set<Contact> contacts = new HashSet<Contact>();
 	  
-	public ContactImpl(String name, String notes){
+	public ContactImpl(int id, String name, String notes){
+		if (id < 1) {
+			throw new IllegalArgumentException("Must be a positive non-zero integer");
+		}
+		
+		if (name == null){
+			throw new NullPointerException("Name cannot be null!");
+		}
+		
+		this.id = id;
 		this.name = name;
 		this.notes = notes;
-		count++;
-		this.id = count;
+		contacts.add(this);
+	}
+	
+	public ContactImpl(int id, String name){
+		this.id = id;
+		this.name = name;
 	}
 	
 	@Override
@@ -31,7 +47,7 @@ public class ContactImpl implements Contact {
 	
 	@Override
 	public void addNotes(String note) {
-		this.notes = note;
+		this.notes += ", " + note;
 	}
 
 }

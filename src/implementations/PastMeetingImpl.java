a3956113ab735080940dc3d1ce7e4e75f90efd55
Hannah Spencer <hspenc03@dcs.bbk.ac.uk>
2017-03-01 13:20:01 +0000

    Add exception to constructor

--- src/implementations/PastMeetingImpl.java
+++ src/implementations/PastMeetingImpl.java
@@ -6,10 +6,16 @@ import interfaces.PastMeeting;
 import java.util.*;
 
 public class PastMeetingImpl extends MeetingImpl implements PastMeeting{
+	//private int id;
+	//private Calendar date;
+	//private Set<Contact> contacts;
 	private String notes;
 	
-	public PastMeetingImpl(Calendar date, Set<Contact> contacts, String notes){
-		super(date,contacts);
+	public PastMeetingImpl(int id, Calendar date, Set<Contact> contacts, String notes) throws Exception{
+		super(id,date,contacts);
+		//this.id = id;
+		//this.date = date;
+		//this.contacts = contacts;
 		this.notes = notes;
 	}
 	

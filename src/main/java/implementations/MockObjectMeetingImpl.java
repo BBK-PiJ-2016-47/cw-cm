a0db108ae37fef7599f5b2eeede82179b609b4f2
Hannah Spencer <hspenc03@dcs.bbk.ac.uk>
2017-03-12 16:21:12 +0000

    adding ID to mockobject

--- src/main/java/implementations/MockObjectMeetingImpl.java
+++ src/main/java/implementations/MockObjectMeetingImpl.java
@@ -7,6 +7,8 @@ import main.java.spec.Contact;
 import main.java.spec.Meeting;
 
 public class MockObjectMeetingImpl extends MeetingImpl implements Meeting{
+  private static final long serialVersionUID = 7041369795030201014L;
+
   public MockObjectMeetingImpl(int id, Calendar date, Set<Contact> contacts) throws Exception {
     super(id, date, contacts);
   }
